edae0e0d43bcae034c953aefbcfff989821534aa
torippy <47879788+torippy1024@users.noreply.github.com>
2023-06-18 13:41:33 +0900

    a-xのコードサンプルを変更

--- src/c.ts
+++ src/c.ts
@@ -2,6 +2,10 @@ import {close, input, inputs} from './utils/stdio';
 
 const main = async () => {
   const N = Number(await input());
+  const A = await inputs(N);
+
+  console.log(`N: ${N}`);
+  console.log(A);
 
   close();
 };
